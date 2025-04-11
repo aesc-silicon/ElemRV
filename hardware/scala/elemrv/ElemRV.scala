94cae96fb3de54265ee3569c2c245875dce52aef
Daniel Schultz <dnltz@aesc-silicon.de>
2025-04-11 15:06:50 +0200

    hardware: Update to ElemRV-N 0.2
    * Reduce clock from 25 to 20 MHz
    * Add pinmuxing with 20 general pins
    * Add more interfaces:
      * I2C Controller
      * PWM
      * PIO
    * Add masked AES accelerator
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- hardware/scala/elemrv/ElemRV.scala
+++ hardware/scala/elemrv/ElemRV.scala
@@ -6,41 +6,128 @@ import spinal.lib.bus.amba3.apb._
 import spinal.lib.bus.amba4.axi._
 
 import zibal.misc._
-import zibal.platform.Hydrogen
+import zibal.platform.Nitrogen
 import zibal.board.BoardParameter
 import zibal.soc.SocParameter
 
 import nafarr.peripherals.io.gpio.{Apb3Gpio, Gpio, GpioCtrl}
+import nafarr.peripherals.io.pio.{Apb3Pio, Pio, PioCtrl}
+import nafarr.peripherals.io.pwm.{Apb3Pwm, Pwm, PwmCtrl}
 import nafarr.peripherals.com.uart.{Apb3Uart, Uart, UartCtrl}
-import nafarr.peripherals.com.i2c.{Apb3I2cController, I2c, I2cCtrl, I2cControllerCtrl}
-import nafarr.peripherals.com.spi.{Apb3SpiController, Spi, SpiCtrl, SpiControllerCtrl}
+import nafarr.peripherals.com.i2c.{Apb3I2cController, I2c, I2cControllerCtrl}
+import nafarr.peripherals.com.spi.{Apb3SpiController, Spi, SpiControllerCtrl}
+import nafarr.peripherals.pinmux.{Apb3Pinmux, Pinmux, PinmuxCtrl}
+import nafarr.crypto.aes.{Apb3AesMaskedAccelerator, AesMaskedAcceleratorCtrl}
 
 object ElemRV {
-  def apply(parameter: Hydrogen.Parameter) = ElemRV(parameter)
+  def apply(parameter: Nitrogen.Parameter) = ElemRV(parameter)
 
-  case class Parameter(boardParameter: BoardParameter) extends SocParameter(boardParameter, 2) {
-    val uartStd = UartCtrl.Parameter.full()
-    val gpioStatus = GpioCtrl.Parameter(Gpio.Parameter(4), 3, null, null, null)
+  case class Parameter(boardParameter: BoardParameter)
+      extends SocParameter(boardParameter, socInterrupts = 6) {
+    val gpio0 = GpioCtrl.Parameter(Gpio.Parameter(20), 3, null, null, null)
+    val i2c0 = I2cControllerCtrl.Parameter.default(1)
+    val i2c1 = I2cControllerCtrl.Parameter.lightweight()
+    val pio0 = PioCtrl.Parameter.default(3)
+    val pwm0 = PwmCtrl.Parameter.default(2)
+    val spi0 = SpiControllerCtrl.Parameter.default()
+    val uart0 = UartCtrl.Parameter.full()
+    val uart1 = UartCtrl.Parameter.lightweight()
+    val aes = AesMaskedAcceleratorCtrl.Parameter.default()
+    val pinmux = PinmuxCtrl.Parameter(Pinmux.Parameter(20), 40, 2)
   }
 
-  case class ElemRV(parameter: Hydrogen.Parameter) extends Hydrogen.Hydrogen(parameter) {
+  case class ElemRV(parameter: Nitrogen.Parameter) extends Nitrogen.Nitrogen(parameter) {
     var socParameter = parameter.getSocParameter.asInstanceOf[Parameter]
-    val io_per = new Bundle {
-      val uartStd = master(Uart.Io(socParameter.uartStd))
-      val gpioStatus = Gpio.Io(socParameter.gpioStatus.io)
+    val io = new Bundle {
+      val pins = Pinmux.Io(socParameter.pinmux.io)
     }
 
     val peripherals = new ClockingArea(clockCtrl.getClockDomainByName("system")) {
 
-      val uartStdCtrl = Apb3Uart(socParameter.uartStd)
-      uartStdCtrl.io.uart <> io_per.uartStd
-      addApbDevice(uartStdCtrl.io.bus, 0x00000, 4 kB)
-      addInterrupt(uartStdCtrl.io.interrupt)
+      val gpio0Ctrl = Apb3Gpio(socParameter.gpio0)
+      addApbDevice(gpio0Ctrl.io.bus, 0x0000, 4 kB)
+      addInterrupt(gpio0Ctrl.io.interrupt)
+      for (pin <- 0 until socParameter.gpio0.io.width) {
+        addPinmuxInput(gpio0Ctrl.io.gpio.pins(pin), s"gpio0_$pin")
+      }
 
-      val gpioStatusCtrl = Apb3Gpio(socParameter.gpioStatus)
-      gpioStatusCtrl.io.gpio <> io_per.gpioStatus
-      addApbDevice(gpioStatusCtrl.io.bus, 0x1000, 4 kB)
-      addInterrupt(gpioStatusCtrl.io.interrupt)
+      val i2c0Ctrl = Apb3I2cController(socParameter.i2c0)
+      addApbDevice(i2c0Ctrl.io.bus, 0x1000, 4 kB)
+      addInterrupt(i2c0Ctrl.io.interrupt)
+      addPinmuxInput(i2c0Ctrl.io.i2c.scl, "i2c0_scl")
+      addPinmuxInput(i2c0Ctrl.io.i2c.sda, "i2c0_sda")
+      addPinmuxInput(i2c0Ctrl.io.i2c.interrupts(0), "i2c0_interrupt_0", output = false)
+
+      val i2c1Ctrl = Apb3I2cController(socParameter.i2c1)
+      addApbDevice(i2c1Ctrl.io.bus, 0x2000, 4 kB)
+      addInterrupt(i2c1Ctrl.io.interrupt)
+      addPinmuxInput(i2c1Ctrl.io.i2c.scl, "i2c1_scl")
+      addPinmuxInput(i2c1Ctrl.io.i2c.sda, "i2c1_sda")
+
+      val pio0Ctrl = Apb3Pio(socParameter.pio0)
+      addApbDevice(pio0Ctrl.io.bus, 0x3000, 4 kB)
+      for (pin <- 0 until socParameter.pio0.io.width) {
+        addPinmuxInput(pio0Ctrl.io.pio.pins(pin), s"pio0_$pin")
+      }
+
+      val pwm0Ctrl = Apb3Pwm(socParameter.pwm0)
+      addApbDevice(pwm0Ctrl.io.bus, 0x4000, 4 kB)
+      for (pin <- 0 until socParameter.pwm0.io.channels) {
+        addPinmuxInput(pwm0Ctrl.io.pwm.output(pin), s"pwm0_$pin")
+      }
+
+      val spi0Ctrl = Apb3SpiController(socParameter.spi0)
+      addApbDevice(spi0Ctrl.io.bus, 0x5000, 4 kB)
+      addInterrupt(spi0Ctrl.io.interrupt)
+      addPinmuxInput(spi0Ctrl.io.spi.cs(0), "spi0_cs0")
+      addPinmuxInput(spi0Ctrl.io.spi.sclk, "spi0_sclk")
+      addPinmuxInput(spi0Ctrl.io.spi.dq(0), "spi0_dq0")
+      addPinmuxInput(spi0Ctrl.io.spi.dq(1), "spi0_dq1")
+
+      val uart0Ctrl = Apb3Uart(socParameter.uart0)
+      addApbDevice(uart0Ctrl.io.bus, 0x6000, 4 kB)
+      addInterrupt(uart0Ctrl.io.interrupt)
+      addPinmuxInput(uart0Ctrl.io.uart.txd, "uart0_tx")
+      addPinmuxInput(uart0Ctrl.io.uart.rxd, "uart0_rx", output = false)
+      addPinmuxInput(uart0Ctrl.io.uart.cts, "uart0_cts", output = false)
+      addPinmuxInput(uart0Ctrl.io.uart.rts, "uart0_rts")
+
+      val uart1Ctrl = Apb3Uart(socParameter.uart1)
+      addApbDevice(uart1Ctrl.io.bus, 0x7000, 4 kB)
+      addInterrupt(uart1Ctrl.io.interrupt)
+      addPinmuxInput(uart1Ctrl.io.uart.txd, "uart1_tx")
+      addPinmuxInput(uart1Ctrl.io.uart.rxd, "uart1_rx", output = false)
+      uart1Ctrl.io.uart.cts := True
+
+      val aesCtrl = Apb3AesMaskedAccelerator(socParameter.aes)
+      addApbDevice(aesCtrl.io.bus, 0x30000, 64 kB)
+
+      /* Pin Mapping */
+      addPinmuxOption(0, List("gpio0_0", "i2c0_scl"))
+      addPinmuxOption(1, List("gpio0_1", "i2c0_sda"))
+      addPinmuxOption(2, List("gpio0_2", "i2c0_interrupt_0"))
+      addPinmuxOption(3, List("gpio0_3", "i2c1_scl"))
+      addPinmuxOption(4, List("gpio0_4", "i2c1_sda"))
+      addPinmuxOption(5, List("gpio0_5", "pio0_0"))
+      addPinmuxOption(6, List("gpio0_6", "pio0_1"))
+      addPinmuxOption(7, List("gpio0_7", "pio0_2"))
+      addPinmuxOption(8, List("gpio0_8", "pwm0_0"))
+      addPinmuxOption(9, List("gpio0_9", "pwm0_1"))
+      addPinmuxOption(10, List("gpio0_10", "spi0_cs0"))
+      addPinmuxOption(11, List("gpio0_11", "spi0_sclk"))
+      addPinmuxOption(12, List("gpio0_12", "spi0_dq0"))
+      addPinmuxOption(13, List("gpio0_13", "spi0_dq1"))
+      addPinmuxOption(14, List("uart0_tx", "gpio0_14"))
+      addPinmuxOption(15, List("uart0_rx", "gpio0_15"))
+      addPinmuxOption(16, List("uart0_cts", "gpio0_16"))
+      addPinmuxOption(17, List("uart0_rts", "gpio0_17"))
+      addPinmuxOption(18, List("gpio0_18", "uart1_tx"))
+      addPinmuxOption(19, List("gpio0_19", "uart1_rx"))
+
+      val pinmuxCtrl = Apb3Pinmux(socParameter.pinmux, getPinmuxMapping())
+      addApbDevice(pinmuxCtrl.io.bus, 0x10000, 4 kB)
+      io.pins <> pinmuxCtrl.io.pins
+      connectPinmuxInputs(pinmuxCtrl)
 
       connectPeripherals()
     }
--- hardware/scala/elemrv/SG13G2/SG13G2.scala
+++ hardware/scala/elemrv/SG13G2/SG13G2.scala
@@ -9,9 +9,10 @@ import nafarr.system.reset.ResetControllerCtrl._
 import nafarr.system.clock._
 import nafarr.system.clock.ClockControllerCtrl._
 import nafarr.blackboxes.ihp.sg13g2._
+import nafarr.memory.ocram.ihp.sg13g2.Axi4SharedIhpOnChipRam
 
 import zibal.misc._
-import zibal.platform.Hydrogen
+import zibal.platform.Nitrogen
 import zibal.board.{KitParameter, BoardParameter}
 import zibal.sim.hyperram.W956A8MBYA
 import zibal.sim.MT25Q
@@ -23,13 +24,8 @@ case class SG13G2Board() extends Component {
   val io = new Bundle {
     val clock = inout(Analog(Bool))
     val reset = inout(Analog(Bool))
-    val uartStd = new Bundle {
-      val txd = inout(Analog(Bool))
-      val rxd = inout(Analog(Bool))
-    }
-    val gpioStatus = Vec(inout(Analog(Bool())), 4)
     val hyperbus = new Bundle {
-      val cs = inout(Analog(Bool))
+      val cs = Vec(inout(Analog(Bool)), 2)
       val ck = inout(Analog(Bool))
       val reset = inout(Analog(Bool))
       val rwds = inout(Analog(Bool))
@@ -41,6 +37,7 @@ case class SG13G2Board() extends Component {
       val mosi = inout(Analog(Bool))
       val miso = inout(Analog(Bool))
     }
+    val pins = Vec(inout(Analog(Bool())), 20)
   }
 
   val top = SG13G2Top()
@@ -52,20 +49,17 @@ case class SG13G2Board() extends Component {
   top.io.clock.PAD := io.clock
   top.io.reset.PAD := io.reset
 
-  top.io.uartStd.rxd.PAD := io.uartStd.rxd
-  io.uartStd.txd := top.io.uartStd.txd.PAD
-  top.io.uartStd.cts.PAD := analogFalse
-  analogFalse := top.io.uartStd.rts.PAD
-
   top.io.jtag.tms.PAD := analogFalse
   top.io.jtag.tck.PAD := analogFalse
   top.io.jtag.tdi.PAD := analogFalse
   analogFalse := top.io.jtag.tdo.PAD
-  for (index <- 0 until top.io.gpioStatus.length) {
-    io.gpioStatus(index) <> top.io.gpioStatus(index).PAD
+
+  for (index <- 0 until top.io.forFutureUse.length) {
+    top.io.forFutureUse(index).PAD := analogFalse
   }
 
   val w956a8mbya = W956A8MBYA()
+  w956a8mbya.io.clock := io.clock
   w956a8mbya.io.ck := io.hyperbus.ck
   w956a8mbya.io.ckN := analogFalse
   for (index <- 0 until top.io.hyperbus.dq.length) {
@@ -74,10 +68,11 @@ case class SG13G2Board() extends Component {
   }
   w956a8mbya.io.rwdsIn := io.hyperbus.rwds
   io.hyperbus.rwds := w956a8mbya.io.rwdsOut
-  w956a8mbya.io.csN := io.hyperbus.cs
+  w956a8mbya.io.csN := io.hyperbus.cs(0)
   w956a8mbya.io.resetN := io.hyperbus.reset
 
-  io.hyperbus.cs := top.io.hyperbus.cs(0).PAD
+  top.io.hyperbus.cs(1).PAD := analogFalse
+  io.hyperbus.cs(0) := top.io.hyperbus.cs(0).PAD
   io.hyperbus.ck := top.io.hyperbus.ck.PAD
   io.hyperbus.reset := top.io.hyperbus.reset.PAD
   io.hyperbus.rwds <> top.io.hyperbus.rwds.PAD
@@ -96,22 +91,30 @@ case class SG13G2Board() extends Component {
   io.spi.sck := top.io.spi.sck.PAD
   io.spi.mosi := top.io.spi.dq(0).PAD
   top.io.spi.dq(1).PAD := io.spi.miso
+  top.io.spi.dq(2).PAD := analogFalse
+  top.io.spi.dq(3).PAD := analogFalse
+
+  for (index <- 0 until top.io.pins.length) {
+    io.pins(index) <> top.io.pins(index).PAD
+  }
 }
 
 case class SG13G2Top() extends Component {
   val resets = List[ResetParameter](ResetParameter("system", 128), ResetParameter("debug", 128))
   val clocks = List[ClockParameter](
-    ClockParameter("system", 100 MHz, "system"),
+    ClockParameter("system", 20 MHz, "system"),
     ClockParameter("debug", 10 MHz, "debug", synchronousWith = "system")
   )
   val hyperbusPartitions = List[(BigInt, Boolean)](
-    (8 MB, false)
+    (8 MB, true),
+    (8 MB, true)
   )
   val kitParameter = KitParameter(resets, clocks)
   val boardParameter = ElemRVBoard.Parameter(kitParameter)
   val socParameter = ElemRV.Parameter(boardParameter)
-  val parameter = Hydrogen.Parameter(
+  val parameter = Nitrogen.Parameter(
     socParameter,
+    32,
     8 MB,
     hyperbusPartitions,
     (resetCtrl: ResetControllerCtrl, reset: Bool, _) => {
@@ -119,36 +122,34 @@ case class SG13G2Top() extends Component {
     },
     (clockCtrl: ClockControllerCtrl, resetCtrl: ResetControllerCtrl, clock: Bool) => {
       clockCtrl.buildDummy(clock)
+    },
+    (ramSize: BigInt) => {
+      val ram = Axi4SharedIhpOnChipRam.OnePort1024x8(
+        dataWidth = 32,
+        byteCount = ramSize,
+        idWidth = 4
+      )
+      (ram, ram.io.axi, ram.engine.ram)
     }
   )
 
   val io = new Bundle {
-    val clock = IhpCmosIo("east", 5)
-    val reset = IhpCmosIo("east", 6)
-    val uartStd = new Bundle {
-      val txd = IhpCmosIo("south", 10)
-      val rxd = IhpCmosIo("south", 8)
-      val cts = IhpCmosIo("south", 9)
-      val rts = IhpCmosIo("south", 11)
-    }
+    val clock = IhpCmosIo("south", 13)
+    val reset = IhpCmosIo("south", 12)
     val jtag = new Bundle {
-      val tms = IhpCmosIo("south", 4)
-      val tdi = IhpCmosIo("south", 5)
-      val tdo = IhpCmosIo("south", 6)
-      val tck = IhpCmosIo("south", 7)
+      val tms = IhpCmosIo("west", 2)
+      val tdi = IhpCmosIo("west", 3)
+      val tdo = IhpCmosIo("west", 4)
+      val tck = IhpCmosIo("west", 5)
     }
-    val gpioStatus = Vec(
-      IhpCmosIo("east", 4),
-      IhpCmosIo("west", 8),
-      IhpCmosIo("west", 9),
-      IhpCmosIo("west", 10)
-    )
     val hyperbus = new Bundle {
       val cs = Vec(
-        IhpCmosIo("east", 8)
+        IhpCmosIo("north", 12),
+        IhpCmosIo("north", 13)
       )
-      val ck = IhpCmosIo("east", 10)
-      val reset = IhpCmosIo("east", 7)
+      val ck = IhpCmosIo("east", 13)
+      val reset = IhpCmosIo("north", 10)
+      val rwds = IhpCmosIo("north", 11)
       val dq = Vec(
         IhpCmosIo("north", 2),
         IhpCmosIo("north", 3),
@@ -159,18 +160,46 @@ case class SG13G2Top() extends Component {
         IhpCmosIo("north", 8),
         IhpCmosIo("north", 9)
       )
-      val rwds = IhpCmosIo("east", 9)
     }
     val spi = new Bundle {
       val cs = Vec(
-        IhpCmosIo("west", 4)
+        IhpCmosIo("east", 2)
       )
+      val sck = IhpCmosIo("east", 3)
       val dq = Vec(
-        IhpCmosIo("west", 6),
-        IhpCmosIo("west", 7)
+        IhpCmosIo("east", 7),
+        IhpCmosIo("east", 6),
+        IhpCmosIo("east", 5),
+        IhpCmosIo("east", 4)
       )
-      val sck = IhpCmosIo("west", 5)
     }
+    val pins = Vec(
+      IhpCmosIo("west", 8),
+      IhpCmosIo("west", 9),
+      IhpCmosIo("west", 10),
+      IhpCmosIo("west", 11),
+      IhpCmosIo("west", 12),
+      IhpCmosIo("west", 13),
+      IhpCmosIo("east", 8),
+      IhpCmosIo("east", 9),
+      IhpCmosIo("east", 10),
+      IhpCmosIo("east", 11),
+      IhpCmosIo("south", 2),
+      IhpCmosIo("south", 3),
+      IhpCmosIo("south", 4),
+      IhpCmosIo("south", 5),
+      IhpCmosIo("south", 6),
+      IhpCmosIo("south", 7),
+      IhpCmosIo("south", 8),
+      IhpCmosIo("south", 9),
+      IhpCmosIo("south", 10),
+      IhpCmosIo("south", 11)
+    )
+    val forFutureUse = Vec(
+      IhpCmosIo("east", 12),
+      IhpCmosIo("west", 6),
+      IhpCmosIo("west", 7)
+    )
   }
 
   val soc = ElemRV(parameter)
@@ -182,15 +211,6 @@ case class SG13G2Top() extends Component {
   io.jtag.tdi <> IOPadIn(soc.io_plat.jtag.tdi)
   io.jtag.tdo <> IOPadOut4mA(soc.io_plat.jtag.tdo)
   io.jtag.tck <> IOPadIn(soc.io_plat.jtag.tck)
-
-  io.uartStd.txd <> IOPadOut4mA(soc.io_per.uartStd.txd)
-  io.uartStd.rxd <> IOPadIn(soc.io_per.uartStd.rxd)
-  io.uartStd.cts <> IOPadIn(soc.io_per.uartStd.cts)
-  io.uartStd.rts <> IOPadOut4mA(soc.io_per.uartStd.rts)
-
-  for (index <- 0 until io.gpioStatus.length) {
-    io.gpioStatus(index) <> IOPadInOut4mA(soc.io_per.gpioStatus.pins(index))
-  }
 
   for (index <- 0 until io.hyperbus.cs.length) {
     io.hyperbus.cs(index) <> IOPadOut4mA(soc.io_plat.hyperbus.cs(index))
@@ -210,24 +230,42 @@ case class SG13G2Top() extends Component {
     io.spi.dq(index) <> IOPadInOut4mA(soc.io_plat.spi.dq(index))
   }
 
-  for (index <- 0 until 5) { IOPadIOVdd() }
-  for (index <- 0 until 5) { IOPadIOVss() }
-  for (index <- 0 until 3) { IOPadVdd() }
-  for (index <- 0 until 3) { IOPadVss() }
+  for (index <- 0 until io.pins.length) {
+    io.pins(index) <> IOPadInOut4mA(soc.io.pins.pins(index))
+  }
+
+  for (index <- 0 until io.forFutureUse.length) {
+    io.forFutureUse(index) <> IOPadIn(False)
+  }
+
+  for (index <- 0 until 4) { IOPadIOVdd() }
+  for (index <- 0 until 4) { IOPadIOVss() }
+  for (index <- 0 until 4) { IOPadVdd() }
+  for (index <- 0 until 4) { IOPadVss() }
 }
 
 object SG13G2Generate extends ElementsApp {
-  elementsConfig.genASICSpinalConfig.generateVerilog {
+  val report = elementsConfig.genASICSpinalConfig.generateVerilog {
     val top = SG13G2Top()
     val config = OpenROADTools.IHP.Config(elementsConfig)
     config.generate(
       OpenROADTools.PDKs.IHP.sg13g2,
-      (0, 0, 2015.04, 2014.74),
-      (394.08, 396.92, 1622.4, 1617.86)
+      (0, 0, 2522.4, 2521.26),
+      (394.08, 396.9, 2125.44, 2124.36)
     )
 
+    val macros = OpenROADTools.IHP.Macros(elementsConfig)
+    macros.addMacro(
+      top.soc.system.onChipCtrl.asInstanceOf[Axi4SharedIhpOnChipRam.OnePort1024x8].engine.ram,
+      444.96,
+      448.35,
+      "MX"
+    )
+    macros.addMacro(top.soc.peripherals.aesCtrl.ctrl.ram, 936.48, 448.35, "MX")
+    macros.generate()
+
     val sdc = OpenROADTools.IHP.Sdc(elementsConfig)
-    sdc.addClock(top.io.clock.PAD, 25 MHz, "clk_core")
+    sdc.addClock(top.io.clock.PAD, 20 MHz, "clk_core")
     sdc.addClock(top.io.jtag.tck.PAD, 10 MHz, "clk_jtag")
     sdc.setFalsePath("clk_core", "clk_jtag")
     sdc.generate(top.io)
@@ -235,24 +273,27 @@ object SG13G2Generate extends ElementsApp {
     val io = OpenROADTools.IHP.Io(elementsConfig)
     io.addPad("south", 0, "sg13g2_IOPadIOVdd")
     io.addPad("south", 1, "sg13g2_IOPadIOVss")
-    io.addPad("south", 2, "sg13g2_IOPadVss")
-    io.addPad("south", 3, "sg13g2_IOPadVdd")
+    io.addPad("south", 14, "sg13g2_IOPadVss")
+    io.addPad("south", 15, "sg13g2_IOPadVdd")
     io.addPad("east", 0, "sg13g2_IOPadIOVdd")
     io.addPad("east", 1, "sg13g2_IOPadIOVss")
-    io.addPad("east", 2, "sg13g2_IOPadVss")
-    io.addPad("east", 3, "sg13g2_IOPadVdd")
-    io.addPad("north", 0, "sg13g2_IOPadIOVdd")
-    io.addPad("north", 1, "sg13g2_IOPadIOVss")
-    io.addPad("north", 10, "sg13g2_IOPadIOVss")
-    io.addPad("north", 11, "sg13g2_IOPadIOVdd")
-    io.addPad("west", 0, "sg13g2_IOPadIOVdd")
-    io.addPad("west", 1, "sg13g2_IOPadIOVss")
-    io.addPad("west", 2, "sg13g2_IOPadVss")
-    io.addPad("west", 3, "sg13g2_IOPadVdd")
+    io.addPad("east", 14, "sg13g2_IOPadVss")
+    io.addPad("east", 15, "sg13g2_IOPadVdd")
+    io.addPad("north", 0, "sg13g2_IOPadVdd")
+    io.addPad("north", 1, "sg13g2_IOPadVss")
+    io.addPad("north", 14, "sg13g2_IOPadIOVss")
+    io.addPad("north", 15, "sg13g2_IOPadIOVdd")
+    io.addPad("west", 0, "sg13g2_IOPadVdd")
+    io.addPad("west", 1, "sg13g2_IOPadVss")
+    io.addPad("west", 14, "sg13g2_IOPadIOVss")
+    io.addPad("west", 15, "sg13g2_IOPadIOVdd")
     io.generate(top.io)
 
     val pdn = OpenROADTools.IHP.Pdn(elementsConfig)
     pdn.generate()
+
+    top.soc.prepareBaremetal("bootrom", elementsConfig)
+    top.soc.prepareBaremetal("demo", elementsConfig)
 
     top
   }
@@ -261,7 +302,8 @@ object SG13G2Generate extends ElementsApp {
 object SG13G2Simulate extends ElementsApp {
   val compiled = elementsConfig.genFPGASimConfig.compile {
     val board = SG13G2Board()
-    BinTools.initRam(board.spiNor.deviceOut.data, elementsConfig.bootromBinary)
+    BinTools.initRam(board.spiNor.deviceOut.data, elementsConfig.swStorageBaremetalImage("bootrom"))
+    BinTools.initRam(board.w956a8mbya.device.data, elementsConfig.swStorageBaremetalImage("demo"))
     board
   }
   simType match {
@@ -274,7 +316,7 @@ object SG13G2Simulate extends ElementsApp {
           simDuration.toString.toInt ms
         )
         testCases.addReset(dut.io.reset, 100 us)
-        testCases.uartRxIdle(dut.io.uartStd.rxd)
+        testCases.uartRxIdle(dut.io.pins(14))
       }
   }
 }
