428eaab5b0d3944b39afe49fc9ce354d571aa265
Daniel Schultz <dnltz@aesc-silicon.de>
2026-05-29 14:54:28 +0200

    Fix Firmware Path
    Firmwares are now stored directly under build/<soc>/firmware because
    there should be no target specific changes. Therefore, they can live
    in a general directory. That also makes handling firmware binaries
    easier when using multiple boards in parallel (like FPGA + ASIC).
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- software/elemrv_h/gen_baremetal_container.sh
+++ software/elemrv_h/gen_baremetal_container.sh
@@ -4,9 +4,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-FW_DIR=${BUILD_ROOT}/${SOC}/${BOARD}/firmware/
+FW_DIR=${BUILD_ROOT}/${SOC}/firmware/
 IMG_CONTAINER=${FW_DIR}/baremetal_container.img
 
 dd if=/dev/zero of=${IMG_CONTAINER} bs=32M count=1
 dd if=${FW_DIR}/bootrom/kernel.img of=${IMG_CONTAINER} conv=notrunc
 dd if=${FW_DIR}/demo/kernel.img of=${IMG_CONTAINER} seek=64 bs=1k conv=notrunc
+echo "Generated ${IMG_CONTAINER}"
