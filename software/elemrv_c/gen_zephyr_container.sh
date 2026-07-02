b0d414be7b282cb42ebef09ac4e801ba18c99731
Daniel Schultz <dnltz@aesc-silicon.de>
2026-07-02 13:59:15 +0200

    software: Generate 512kb images
    The total flash size is defined with 512kB in Zephyr.
    Generate images which fit this image size and truncate them
    later in case larger images are required.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- software/elemrv_c/gen_zephyr_container.sh
+++ software/elemrv_c/gen_zephyr_container.sh
@@ -7,7 +7,7 @@
 FW_DIR=${BUILD_ROOT}/${SOC}/firmware/
 IMG_CONTAINER=${FW_DIR}/zephyr_container.img
 
-dd if=/dev/zero of=${IMG_CONTAINER} bs=32M count=1
+dd if=/dev/zero of=${IMG_CONTAINER} bs=1k count=512
 dd if=${FW_DIR}/bootrom/kernel.img of=${IMG_CONTAINER} conv=notrunc
 dd if=${FW_DIR}/zephyr/zephyr/zephyr.bin of=${IMG_CONTAINER} seek=64 bs=1k conv=notrunc
 echo "Generated ${IMG_CONTAINER}"
